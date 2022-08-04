7811fbb37964e427febbdb3eff5e7548288d4432
zadrogg <koreecrulit@gmail.com>
2022-08-04 11:44:30 +0300

    [master][task] combine key and value

---
+++ 4.php
@@ -0,0 +1,15 @@
+<?php  
+
+$array = [
+    ['id' => 1, 'date' => "12.01.2020", 'name' => "test1"],
+    ['id' => 2, 'date' => "02.05.2020", 'name' => "test2"],
+    ['id' => 4, 'date' => "08.03.2020", 'name' => "test4"],
+    ['id' => 1, 'date' => "22.01.2020", 'name' => "test1"],
+    ['id' => 2, 'date' => "11.11.2020", 'name' => "test4"],
+    ['id' => 3, 'date' => "06.06.2020", 'name' => "test3"],
+];
+
+$id = array_unique(array_column($array, 'id'));
+$name = array_unique(array_column($array, 'name'));
+
+print_r(array_combine($name, $id));
