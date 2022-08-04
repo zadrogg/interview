3c1360569a0d9094c668ac3ad1e03145125207fe
zadrogg <koreecrulit@gmail.com>
2022-08-04 09:50:59 +0300

    [master][task] first

---
+++ 1.php
@@ -0,0 +1,14 @@
+<?php 
+
+$array = [
+    ['id' => 1, 'date' => "12.01.2020", 'name' => "test1"],
+    ['id' => 2, 'date' => "02.05.2020", 'name' => "test2"],
+    ['id' => 4, 'date' => "08.03.2020", 'name' => "test4"],
+    ['id' => 1, 'date' => "22.01.2020", 'name' => "test1"],
+    ['id' => 2, 'date' => "11.11.2020", 'name' => "test4"],
+    ['id' => 3, 'date' => "06.06.2020", 'name' => "test3"],
+];
+
+print_r(array_unique(array_column($array, 'id')));
+
+?>
